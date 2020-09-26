8000cfe6d2ea3953c10bd66a88bbd39692edf2b7
Alola-David <aloladavid20@gmail.com>
2020-09-26 11:16:03 +0100

    First commit

---
+++ src/test/java/MonosnapLogin/Login.java
@@ -0,0 +1,48 @@
+package MonosnapLogin;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Login {
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+
+        driver.get("https://monosnap.com/");
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.manage().window().maximize();
+
+        System.out.println(driver.getTitle());
+
+        //Click Signin Button
+        driver.findElement(By.xpath("//*[@id=\"root\"]/div[2]/div/div[3]/div[1]/div/div[2]/div[3]/span")).click();
+
+        //Locate Email field
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/form/div[4]/input[1]")).sendKeys("aloladavid20@gmail.com");
+
+        //Locate password field
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/form/div[4]/input[2]")).sendKeys("PasswordUiTesting");
+
+
+        //Click on the Sign in button
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/div[1]/div[1]/div/div[2]/form/button")).click();
+
+    }
+    //Close the browser after
+    public static void main(String args[]){
+        Login test = new Login();
+        test.setUp();
+    }
+
+    public void setDriver(WebDriver driver){
+        this.driver = driver;
+        driver.quit();
+    }
+}
